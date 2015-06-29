98802923d20506e9934ec5c806f039aee992e5c7
Amie Wilt <amie.wilt@gmail.com>
2015-06-29 14:45:49 -0400

    added products, carts and orderitems controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
+  get 'products/index'
+
   root to: 'visitors#index'
   devise_for :users
 end
