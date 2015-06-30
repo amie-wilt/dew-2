e32f26b23fb22b8ba9453dbbc7a9eb1e70310e45
Amie Wilt <amie.wilt@gmail.com>
2015-06-30 11:49:53 -0400

    added product params to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,14 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :permalink, :price, :image)
+  end
 end
